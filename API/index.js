f33cc9b1b48e31f86846edc4162f3bfedab4d125
Tudor Androne <tudor.androne@student.upt.ro>
2022-10-13 23:13:58 +0300

    created API Schemas and async function for register

---
+++ API/index.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const userRoute = require("./routes/user")
+const authRoute = require("./routes/auth");
+
+dotenv.config();
+
+mongoose.connect(
+    (process.env.MONGO_URL)
+    ).then(()=>console.log("DBConnection Succesfull!"))
+    .catch((err)=>{console.log(err)});
+
+    app.use(express.json());
+    app.use("/api/auth",authRoute);
+    app.use("/api/users",userRoute);
+
+
+app.listen(5000, ()=>{
+    console.log("Backend server running");
+});
